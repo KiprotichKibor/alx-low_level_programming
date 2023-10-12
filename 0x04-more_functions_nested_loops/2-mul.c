1504c95ec8ad77b17485d30bc551fdea72167f79
KiprotichKibor <kiborotich24@gmail.com>
2023-10-12 05:10:00 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - a function that multiplies two integers
+ * @a: prototype parameter 1
+ * @b: prototype parameter 2
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	int i = (a * b);
+
+	return (i);
+}
