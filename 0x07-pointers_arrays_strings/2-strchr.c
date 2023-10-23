1c84f349ee3a4716cf94b32daa3c0aa8b30cff43
KiprotichKibor <kiborotich24@gmail.com>
2023-10-23 13:45:30 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strchr - a function that locates a character in a string
  * @s: string operated
@@ -15,5 +16,5 @@ char *_strchr(char *s, char c)
 	}
 		if (*s == c)
 			return (s);
-		return ("NULL");
+		return (NULL);
 }
