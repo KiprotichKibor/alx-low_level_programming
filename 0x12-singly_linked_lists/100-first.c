c91b999d4103ef8212b78834f31c3e17adc2df9b
KiprotichKibor <kiborotich24@gmail.com>
2023-11-16 16:04:25 -0800

    revised

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,10 @@
 #include "lists.h"
-void BeforeMain(void)__attribute__ ((constructor));
+void BeforeMain(void)__attribute__((constructor));
 /**
  * BeforeMain - a functions that runs before main
  * Return: void
  */
 void BeforeMain(void)
 {
-	printf("You're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
