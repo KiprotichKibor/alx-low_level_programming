e876e3922a7efda80d0c0afb8d8ca264c258148e
KiprotichKibor <kiborotich24@gmail.com>
2023-11-21 15:43:55 -0800

    more update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,11 +8,10 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h == NULL)
-		return (0);
-	for (count = 0; h != NULL; count++)
+	while (h)
 	{
 		printf("%d\n", h->n);
+		count++;
 		h = h->next;
 	}
 	return (count);
